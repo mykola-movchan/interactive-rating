80389c1aec187fc3099a110ac9e469d5e4ff3254
mykola-movchan <m.movchan@udu.edu.ua>
2026-03-17 17:12:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const buttons = document.querySelectorAll('.buttons button');
-console.log(buttons);
+const submit = document.querySelector('.submit');
+const main = document.querySelector('main');
+const thanks = document.querySelector('.thank-you');
+const scoreDisplay = document.querySelector('#score-display');
+const popover = document.querySelector('#popover');
+let score = null;
+
+const eraseScore = () => [...buttons]
+    .filter(btn => btn.classList.contains('chosen'))
+    .forEach(btn => btn.classList.remove('chosen'));
 
 function handleButton(e) {
-    console.log(e.target.textContent);
+    eraseScore();
+    e.target.classList.add('chosen');
+    score = e.target.dataset.score;
+    scoreDisplay.textContent = score;
+    console.log(score);
 }
 
+const displayScore = () => {
+    main.style.display = 'none';
+    thanks.style.display = 'flex';
+    popover.showPopover();
+    setTimeout(() => {
+        eraseScore();
+        thanks.style.display = 'none';
+        main.style.display = 'flex';
+        popover.hidePopover();
+    }, 5000);
+};
+
 buttons.forEach(button => button.addEventListener('click', handleButton));
+submit.addEventListener('click', displayScore);
